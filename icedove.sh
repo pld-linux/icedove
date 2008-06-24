ceb3e54afeaa6180a7e662c862f88fce0b9ab04f
hawk <hawk@pld-linux.org>
2008-06-24 10:15:02 +0000

    - taken from thunderbird 2.0.0.14 for iceweasel 2.0.0.14
    Changed files:
        icedove-enigmail-shared.patch -> 1.1
        icedove-enigmail.manifest -> 1.1
        icedove-fonts.patch -> 1.1
        icedove-gcc.patch -> 1.1
        icedove-lib_path.patch -> 1.1
        icedove-myspell.patch -> 1.1
        icedove-regionNames.patch -> 1.1
        icedove.desktop -> 1.1
        icedove.sh -> 1.1

---
+++ icedove.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+# based on script by (c) vip at linux.pl, wolf at pld-linux.org
+
+LIBDIR="@LIBDIR@/icedove"
+
+MOZARGS=
+MOZLOCALE="$(/usr/bin/locale | grep "^LC_MESSAGES=" | \
+		sed -e "s|LC_MESSAGES=||g" -e "s|\"||g" )"
+for MOZLANG in $(echo $LANGUAGE | tr ":" " ") $MOZLOCALE; do
+	eval MOZLANG="$(echo $MOZLANG | sed -e "s|_\([^.]*\).*|-\1|g")"
+
+	if [ -f $LIBDIR/chrome/$MOZLANG.jar ]; then
+		MOZARGS="-UILocale $MOZLANG"
+		break
+	fi
+done
+
+if [ -z "$MOZARGS" ]; then
+	# try harder
+	for MOZLANG in $(echo $LANGUAGE | tr ":" " ") $MOZLOCALE; do
+		eval MOZLANG="$(echo $MOZLANG | sed -e "s|_.*||g")"
+
+		LANGFILE=$(echo ${MOZILLA_FIVE_HOME}/chrome/${MOZLANG}*.jar \
+				| sed 's/\s.*//g' )
+		if [ -f "$LANGFILE" ]; then
+			MOZLANG=$(basename "$LANGFILE" | sed 's/\.jar//')
+			MOZARGS="-UILocale $MOZLANG"
+			break
+		fi
+	done
+fi
+
+if [ -n "$MOZARGS" ]; then
+	ICEDOVE="$LIBDIR/icedove $MOZARGS"
+else
+	ICEDOVE="$LIBDIR/icedove"
+fi
+
+if [ "$1" == "-remote" ]; then
+	$ICEDOVE "$@"
+else
+	PING=`$ICEDOVE -remote 'ping()' 2>&1 >/dev/null`
+	if [ -n "$PING" ]; then
+		$ICEDOVE "$@"
+	else
+		case "$1" in
+		    -compose|-editor)
+			$ICEDOVE -remote 'xfeDoCommand (composeMessage)'
+			;;
+		    *)
+			$ICEDOVE -remote 'xfeDoCommand (openInbox)'
+			;;
+		esac
+	fi
+fi
